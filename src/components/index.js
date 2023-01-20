6bb5d34a444df58c9f6a346da93827b396cc1d7a
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-01-20 10:09:23 +0600

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article } from './article/Article';
+export {default as Brand  } from './brand/Brand';
+export {default as CTA } from './cta/cta';
+export {default as feature } from './feature/feature';
+export {default as Navbar} from './navbar/navbar';
